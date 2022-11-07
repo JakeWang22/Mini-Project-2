754237041c15e2d416dcd5d2db4449f50fb53716
JakeWang22 <jakewang2678@gmail.com>
2022-11-07 17:39:46 -0500

    initial changes

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+     <h1>Cat Facts</h1>
+     <p>Current Cat Fact</p>
+     <button>New Fact</button>
+     <p1>Previous Cat</p1>
+    </div>
+  );
+}
+
+export default App;
